de4b0511a8ed7cc27590be5773ae8f65a160f006
Adriano Sepe <adrianosepe@hotmail.com>
2014-04-15 20:06:54 -0300

    Desenvolvimento do exerício do Deitel!

---
+++ InvoiceOO/InvoiceOO/Invoice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceOO
+{
+    public class Invoice
+    {
+        private int numeroItem;
+        private String descricao;
+        private int quantidade;
+        private double preco;
+
+
+        public Invoice(int numeroItem, String descricao,
+            int quantidade, double preco)
+        {
+            if (quantidade < 0)
+            {
+                quantidade = 0;
+            }
+
+            if (preco < 0)
+            {
+                preco = 0;
+            }
+
+            this.numeroItem = numeroItem;
+            this.descricao = descricao;
+            this.quantidade = quantidade;
+            this.preco = preco;
+        }
+
+        public int NumeroItem
+        {
+            get { return numeroItem; }
+            set { numeroItem = value; }
+        }
+
+        public String Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+        public int Quantidade
+        {
+            get { return quantidade; }
+            set { quantidade = value; }
+        }
+
+        public double Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+
+        public double GetInvoiceAmount()
+        {
+            return quantidade * preco;
+        }
+    }
+}
---
+++ InvoiceOO/InvoiceOO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice nt1 = new Invoice(
+                1000, "HD Externo Seagate XP22",
+                2, 123.45);
+
+            Console.WriteLine("{0} - {1} - {2} - {3:c}",
+                nt1.NumeroItem, nt1.Descricao, nt1.Quantidade,
+                nt1.Preco);
+            Console.WriteLine("Total: {0:c}", nt1.GetInvoiceAmount());
+            
+        }
+    }
+}
