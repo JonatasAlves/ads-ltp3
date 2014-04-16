a5edb0f54746b36867f06964c94a56896d1b9902
Adriano Sepe <adrianosepe@hotmail.com>
2014-04-16 20:17:58 -0300

    Revi

---
+++ Ponto2DOO/Ponto2DOO/Ponto2D.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponto2DOO
+{
+    public class Ponto2D
+    {
+        // Atributos X, Y
+        private double x;
+        private double y;
+
+        public Ponto2D()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public Ponto2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Ponto2D(Ponto2D ponto)
+        {
+            x = ponto.x;
+            y = ponto.y;
+        }
+
+        public double GetX()
+        {
+            return x;
+        }
+
+        public void SetX(double x)
+        {
+            this.x = x;
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public void Movimentar()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public void Movimentar(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void Movimentar(Ponto2D ponto)
+        {
+            this.x = ponto.x;
+            this.y = ponto.y;
+        }
+
+        public bool Equals(Ponto2D ponto)
+        {
+            return ponto.x == x && ponto.y == y;
+            /*
+            if(ponto.x == x && ponto.y == y)
+                return true;
+            else
+                return false;
+            */
+        }
+
+        public String ToString()
+        {
+            return "(" + x + ", " + y + ")";
+            // printf("(%f, %f)", x, y);
+        }
+
+        public double Distancia(Ponto2D ponto)
+        {
+            return
+                Math.Sqrt(
+                    Math.Pow(ponto.x - x, 2) +
+                    Math.Pow(ponto.y - y, 2));
+        }
+
+        public Ponto2D Clone()
+        {
+            return new Ponto2D(this);
+        }
+    }
+}
---
+++ Ponto2DOO/Ponto2DOO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponto2DOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ponto2D p1 = new Ponto2D();
+            Ponto2D p2 = new Ponto2D(5, 5);
+            Ponto2D p3 = new Ponto2D(p2);
+            Ponto2D p4 = p3.Clone();
+
+            double distancia = p1.Distancia(p4);
+
+            Console.WriteLine("Ponto 1: {0}", p1.ToString());
+            Console.WriteLine("Ponto 2: {0}", p2.ToString());
+            Console.WriteLine("Ponto 3: {0}", p3.ToString());
+            Console.WriteLine("Ponto 4: {0}", p4.ToString());
+
+            Console.WriteLine(
+                "Distância: {0}", distancia);
+
+            Console.ReadKey();
+        }
+    }
+}
